55e83430ebc3e211c32333784236feed5d9b95fd
tomrath <30604995+tomrath@users.noreply.github.com>
2020-01-08 17:48:01 -0700

    Add files via upload

---
+++ PersonCard.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.props.firstName} {this.props.lastName}
+        </h1>
+        <p>Age: {this.props.age}</p>
+        <p>Hair Color: {this.props.hairColor}</p>
+      </div>
+    );
+  }
+}
+
+export default PersonCard;
